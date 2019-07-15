ce725f282841739c16ee9a0010849a15dbec61ce
nzukimtshazi <48057729+nzukimtshazi@users.noreply.github.com>
2019-07-15 22:53:44 +0200

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,6 +8,7 @@ use App\Models\User\User;
 use Session;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Routes;
 
 class UserController extends Controller
@@ -46,17 +47,13 @@ class UserController extends Controller
 
         $exists = User::where('email', $user->email)->first();
         if ($exists) {
-            $user_id = $exists->id;
-            return view('contact.create', compact('user_id'));
-            //return Redirect::route('contact.create')->withInput()->with('danger', 'User with email "' . $user->email . '" already exists!');
+            $message = 'User already exists, please login!';
+            return Redirect::back()->withInput()->withErrors(array('email' => $message));
         }
 
         if ($user->save()) {
-            dd('User created');
-            return Redirect::route('contact.create')->with('success', 'Successfully added user!');
-        } else {
-            dd('User not created');
-            return Redirect::route('user.create')->withInput()->withErrors($user->errors());
+            $message = 'Successfully added user!';
+            return Redirect::back()->withInput()->withErrors(array('email' => $message));
         }
     }
 
@@ -92,5 +89,30 @@ class UserController extends Controller
     public function destroy($id)
     {
         //
+    }
+    public function showLogin()
+    {
+        // show the form
+        return view('user.login');
+    }
+
+    public function doLogin()
+    {
+        // validate the info, create rules for the inputs
+        $user = User::where('email', '=', Input::get('email'))->first();
+
+        //check if email address exists
+        if (!$user) {
+            $message = "Email address does not exist on user's table, please register";
+            return Redirect::back()->withInput()->withErrors(array('email' => $message));
+        } else {
+            $user_id = $user->id;
+            return Redirect::route('contacts', ['user_id' => $user_id]);
+        }
+    }
+    public function doLogout()
+    {
+        Auth::logout(); // log the user out of our application
+        return view('user.login');
     }
 }
