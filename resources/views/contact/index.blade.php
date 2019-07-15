bd790c4538e6c68fccbcc2726162882a67bf0124
nzukimtshazi <48057729+nzukimtshazi@users.noreply.github.com>
2019-07-15 22:49:18 +0200

    Update index.blade.php

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -14,21 +14,25 @@
                 <h4>Contacts</h4>
             </div>
             <div class="col-xs-6 text-right">
-                <a href="contact/create" role="button" class="btn btn-default">Add Contact</a>
+
+                <a href="{!!URL::route('contact.create',['user_id' => $user_id])!!}" class="btn btn-warning">Add Contact</a>
+                <button type="button" class="pull-right glyphicon glyphicon-tag" style="background-color: Transparent;border: none; " data-toggle="modal" data-target="#searchModal">Search</button>
+                {{--<a href="{!!URL::route('contact.search')!!}" class="btn btn-default">Search</a>--}}
+                <a href="{!!URL::route('user.login')!!}" class="btn btn-default">Back</a>
             </div>
         </div>
-    </div>
 
     <div class="panel-body">
         <table class="table table-striped" id="dataTable">
+
         @if (count($contacts) > 0)
 
             <!-- Table Headings -->
                 <thead>
-                <th width="15%">Name</th>
-                <th width="15%">Surname</th>
-                <th width="25%">Email</th>
-                <th width="15%">Contact No</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Contact No</th>
                 <th width="*">Action</th>
                 </thead>
 
@@ -56,36 +60,26 @@
                         </td>
 
                         <td>
-                            <div class="row">
-                                <div class="col-xs-12 col-sm-4 col-md-2">
-                                    {!! Form::model($contact, ['method' => 'GET', 'route' => ['contact.edit', $contact->id]]) !!}
-                                    <button type="submit" class="btn btn-warning">
-                                        <i class="fa fa-trash"></i> Edit
-                                    </button>
-                                    {!! Form::close() !!}
-                                </div>
-                                <div class="col-xs-12 col-sm-4 col-md-1">
-                                    {!! Form::model($contact, ['method' => 'patch', 'route' => ['contact.delete', $contact->id]]) !!}
-                                    <button type="submit" class="btn  btn-danger">
-                                        <i class="fa fa-trash"></i> Delete
-                                    </button>
-                                    {!! Form::close() !!}
+                            <div class="col-md-12">
+                                <div class="col-md-6">
+                                    <a href="{!!URL::route('contact.edit',[$contact->id, 'user_id' => $user_id])!!}" class="btn btn-warning">Edit</a>
+                                    <a href="{!!URL::route('contact.delete',[$contact->id, 'user_id' => $user_id])!!}" class="fa fas_trash">Delete</a>
                                 </div>
                             </div>
                         </td>
                     </tr>
                 @endforeach
-                <div class="row">
-                    <div class="col-sm-12 col-md-12">
-                        <div class="col-sm-4 col-md-4">
-                            {{ Form::hidden('user_id', $user_id) }}
-                        </div>
-                    </div>
-                </div>
                 </tbody>
             @else
                 <div class="alert alert-info" role="alert">No contacts are available</div>
             @endif
+            <div class="row">
+                <div class="col-sm-12 col-md-12">
+                    <div class="col-sm-4 col-md-4">
+                        {{ Form::hidden('user_id', $user_id) }}
+                    </div>
+                </div>
+            </div>
         </table>
     </div>
 </div>
